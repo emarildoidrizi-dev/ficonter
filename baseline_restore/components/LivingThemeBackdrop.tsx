c232b59478873394c0ce0c3cf47399a3a21d65ef
emarildoidrizi-dev <emarildo.idrizi@gmail.com>
2026-08-15 10:29:59 +0200

    fix(build): remove stale rollback sources from repository

--- baseline_restore/components/LivingThemeBackdrop.tsx
+++
@@ -1,8 +0,0 @@
-export function LivingThemeBackdrop() {
-  return (
-    <div className="living-theme-backdrop" aria-hidden="true">
-      <span className="living-theme-scene" />
-      <span className="living-theme-readability" />
-    </div>
-  );
-}
